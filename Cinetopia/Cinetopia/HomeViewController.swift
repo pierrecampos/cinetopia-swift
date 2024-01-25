a51f752b3071d69d16787f7c77ad02f51420495d
Pierre Dias <cdias.pierre@gmail.com>
2024-01-24 22:24:12 -0300

    [feat] Tela inicial criada

--- Cinetopia/Cinetopia/HomeViewController.swift
+++ Cinetopia/Cinetopia/HomeViewController.swift
@@ -8,13 +8,76 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: .logo)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var coupleImageView: UIImageView = {
+        let imageView = UIImageView(image: .couple)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "O lugar ideal para buscar, salvar e organizar seus filmes favoritos!"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var welcomeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Quero começar!", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 20)
+        button.setTitleColor(.background, for: .normal)
+        button.backgroundColor = .buttonBackground
+        button.layer.cornerRadius = 32
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .background
+        addSubviews()
+        setupConstraints()
     }
-
-
+    
+    private func addSubviews() {
+        view.addSubview(logoImageView)
+        view.addSubview(coupleImageView)
+        view.addSubview(welcomeLabel)
+        view.addSubview(welcomeButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            coupleImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 32),
+            coupleImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            welcomeLabel.topAnchor.constraint(equalTo: coupleImageView.bottomAnchor, constant: 32),
+            welcomeLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            welcomeLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            welcomeButton.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 32),
+            welcomeButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 64),
+            welcomeButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -64),
+            welcomeButton.heightAnchor.constraint(equalToConstant: 64)
+            
+        ])
+    }
+    
+    
 }
 
