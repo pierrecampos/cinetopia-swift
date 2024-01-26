bc56bfbae27525634c07b51709aeee2836d1e69f
Pierre Dias <cdias.pierre@gmail.com>
2024-01-25 21:53:15 -0300

    [feat] Criado tela Movies, adicionado navegação

---
+++ Cinetopia/Cinetopia/MoviesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MoviesViewController.swift
+//  Cinetopia
+//
+//  Created by Pierre Campos Dias on 25/01/24.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        title = "Filmes Populares"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        navigationItem.setHidesBackButton(true, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
